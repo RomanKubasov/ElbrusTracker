3a15cfa3f990eee4dda5ed19463d9959f7cbceb9
Roman <roman.kubasov@gmail.com>
2022-06-19 13:31:10 +0300

    404 page

--- frontend/src/Components/Page404/Page404.jsx
+++ frontend/src/Components/Page404/Page404.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import style from './Page404.module.css';
 
 function Page404() {
   return (
-    <div>404 Page not found</div>
+    <div className={style.container}>
+      <div className={style.messagebox}>
+        <div className={style.title}>Ошибка 404:</div>
+        <div className={style.subtitle}>Это не та страница, которую вы ищете!</div>
+        <a href="/">Вернуться на главную</a>
+      </div>
+      <img className={style.imagebox} src="/images/404.jpg" alt="404" />
+    </div>
   );
 }
 
