db6e98f0501f9e99bfdd9cdf08f2f9017d9f6fb1
Aleksandr <viking26@mail.ru>
2022-06-20 14:29:49 +0300

    add heroku

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = 3001;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 // const indexRouter = require('./routes/indexRouter');
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
