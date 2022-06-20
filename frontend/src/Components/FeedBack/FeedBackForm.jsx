d48a55ddd23b8d618069fc60b6fb6990cfbcc6c2
Shamil Omarov <s_omarov05@mail.ru>
2022-06-20 18:59:17 +0300

    Feedback buttons

--- frontend/src/Components/FeedBack/FeedBackForm.jsx
+++ frontend/src/Components/FeedBack/FeedBackForm.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import { setFeedBackToUserId } from '../../Redux/actions/feedbackToAction';
-import { getFeedBackMetricsRequest } from '../../Redux/actions/feedbackMetricsAction';
+import { actionClickMetric, getFeedBackMetricsRequest } from '../../Redux/actions/feedbackMetricsAction';
 import { sendFeedBack, clearFeedBack } from '../../Redux/actions/feedbackAction';
 import style from './FeedBackForm.module.css';
 
@@ -12,11 +12,13 @@ function FeedBackForm() {
   const { feedbackTo, feedbackMetrics, feedback } = useSelector((state) => state);
   const dispatch = useDispatch();
   const [inputState, setInputState] = useState({});
+
   function changeTextHandler(e) {
     // for (let i=0;i<feedback.length)
     setInputState({ id: Number(e.target.id), value: e.target.value });
   }
   function buttonPressHandler(el) {
+    dispatch(actionClickMetric(el.id));
     dispatch(sendFeedBack({ id: el.id, value: 'true' }));
   }
   async function submitHandler() {
@@ -49,13 +51,13 @@ function FeedBackForm() {
         <h3 className={style.feedbackForm__titles}>Сильные стороны:</h3>
         <div>
           {feedbackMetrics.filter((el) => el.type_id === 1)
-            .map((el, index) => (<button className={style.feedBackForm__button} type="button" key={index} onClick={() => buttonPressHandler(el)}>{el.metric}</button>))}
+            .map((el, index) => (<button className={`${style.feedBackForm__button} ${el.clicked === true ? style.active : ''}`} type="button" key={index} onClick={() => buttonPressHandler(el)}>{el.metric}</button>))}
         </div>
 
         <h3 className={style.feedbackForm__titles}>Зоны для развития:</h3>
         <div>
           {feedbackMetrics.filter((el) => el.type_id === 2)
-            .map((el, index) => (<button className={style.feedBackForm__button} type="button" key={index} onClick={() => buttonPressHandler(el)}>{el.metric}</button>))}
+            .map((el, index) => (<button className={`${style.feedBackForm__button} ${el.clicked === true ? style.active : ''}`} type="button" key={index} onClick={() => buttonPressHandler(el)}>{el.metric}</button>))}
         </div>
 
         <h3 className={style.feedbackForm__titles}>
@@ -70,13 +72,14 @@ function FeedBackForm() {
               id={el.id}
               onChange={changeTextHandler}
               key={index}
+              placeholder="Введите текст..."
             />
           ))}
       </div>
 
       <div className={style.feedBackForm__buttons}>
-        <button type="button" onClick={() => { dispatch(setFeedBackToUserId(null)); }}>Выйти</button>
-        <button type="button" onClick={submitHandler}>Отправить</button>
+        <button className={style.feedBackForm__buttons__logout} type="button" onClick={() => { dispatch(setFeedBackToUserId(null)); }}>Выйти</button>
+        <button className={style.feedBackForm__buttons__send} type="button" onClick={submitHandler}>Отправить</button>
       </div>
     </>
   );
--- frontend/src/Redux/reducers/feedbackMetricsReducer.js
+++ frontend/src/Redux/reducers/feedbackMetricsReducer.js
@@ -1,5 +1,6 @@
 /* eslint-disable default-param-last */
 import {
+  CLICK_METRIC,
   GET_FEEDBACK_METRICS,
 } from '../types/types';
 
@@ -7,7 +8,10 @@ const feedbackMetricsReducer = (state = [], action) => {
   const { type, value } = action;
   switch (type) {
     case GET_FEEDBACK_METRICS: {
-      return value;
+      return value.map((el) => ({ ...el, clicked: false }));
+    }
+    case CLICK_METRIC: {
+      return state.map((el) => (el.id === value ? ({ ...el, clicked: !el.clicked }) : el));
     }
     default:
       return state;
