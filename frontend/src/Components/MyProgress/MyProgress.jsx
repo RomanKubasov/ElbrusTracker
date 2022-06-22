70ce8d8781edb8d1bf6c85b05b2b12462811aecb
Shamil Omarov <s_omarov05@mail.ru>
2022-06-22 11:41:32 +0300

    Chart MyProgressPage

--- frontend/src/Components/MyProgress/MyProgress.jsx
+++ frontend/src/Components/MyProgress/MyProgress.jsx
@@ -1,51 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {
-  Chart as ChartJS, ArcElement, Tooltip, Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import style from './MyProgress.module.css';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 function MyProgress() {
-  const [dataProgress, setDataProgress] = useState([{ mood: -1, sleep: -1, performance: -1 }]);
+  const [dataProgress, setDataProgress] = useState([{ mood: 0, sleep: 0, performance: 0 }]);
 
   useEffect(() => {
     axios.get('http://localhost:3001/myprogress').then((res) => setDataProgress(res.data));
-  }, []);
-  // console.log(dataProgress[dataProgress.length - 1].mood);
-  // console.log(dataProgress[dataProgress.length - 1].sleep);
-  // console.log(dataProgress[dataProgress.length - 1].performance);
+  }, [dataProgress]);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Показатели:',
+      },
+    },
+  };
+
+  const labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
   const data = {
-    labels: ['Настроение', 'Сон', 'Результат дня'],
+    labels,
     datasets: [
       {
-        label: '# of Votes',
-        data: [
-          dataProgress[dataProgress.length - 1].mood,
-          dataProgress[dataProgress.length - 1].sleep,
-          dataProgress[dataProgress.length - 1].performance,
-        ],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.7)',
-          'rgba(54, 162, 235, 0.7)',
-          'rgba(153, 102, 255, 0.7)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 1,
+        label: 'Mood',
+        data: labels.map((el, index) => dataProgress[index]?.mood),
+        backgroundColor: 'rgba(255, 99, 132, 0.7)',
+      },
+      {
+        label: 'Sleep',
+        data: labels.map((el, index) => dataProgress[index]?.sleep),
+        backgroundColor: 'rgba(54, 162, 235, 0.7)',
+      },
+      {
+        label: 'Result Of Day',
+        data: labels.map((el, index) => dataProgress[index]?.performance),
+        backgroundColor: 'rgba(53, 102, 255, 0.7)',
       },
     ],
   };
 
   return (
     <div className={style.chart}>
-      <Doughnut data={data} />
+      <Bar options={options} data={data} />
     </div>
   );
 }
