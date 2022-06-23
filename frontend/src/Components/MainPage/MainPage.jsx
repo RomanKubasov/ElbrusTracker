741ee668043e6f4f5a4b4729fadbdb2a6659b9c1
Roman <roman.kubasov@gmail.com>
2022-06-23 18:00:48 +0300

    Spinners on LostButton + AuthRouter

--- frontend/src/Components/MainPage/MainPage.jsx
+++ frontend/src/Components/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setIsLoading } from '../../Redux/actions/isLoadingAction';
 import style from './MainPage.module.css';
 import imgProgress from '../../images/progress_img.jpg';
 import imgFeedback from '../../images/feedback.jpg';
@@ -8,35 +9,44 @@ import imgTeacherMonitor from '../../images/teacher_monitor.jpg';
 import Spinner from '../Spinner/Spinner';
 
 function MainPage() {
+  const dispatch = useDispatch();
   const { isLoading, user } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(setIsLoading({ ...isLoading, status: true }));
+    setTimeout(() => dispatch(setIsLoading({ ...isLoading, status: false })), 2000);
+  }, []);
 
   return (
     <>
       {isLoading.status && !user.login && (<Spinner />)}
       <div className={style.mainPage}>
-
-        <div className={style.mainPage__card_progress}>
-          <Link to="/progress" className={style.mainPage__card_progress_inner}>
-            <div className={style.mainPage__card_progress_img}>
-              <img src={imgProgress} alt="Progress_Image" />
+        {(user.role_id === 2) && (
+        <>
+          <div className={style.mainPage__card_progress}>
+            <Link to="/progress" className={style.mainPage__card_progress_inner}>
+              <div className={style.mainPage__card_progress_img}>
+                <img src={imgProgress} alt="Progress_Image" />
+              </div>
+            </Link>
+            <div className={style.mainPage__card_progress_info}>
+              <p>Мой прогресс</p>
             </div>
-          </Link>
-          <div className={style.mainPage__card_progress_info}>
-            <p>Мой прогресс</p>
           </div>
-        </div>
 
-        <div className={style.mainPage__card_feedback}>
-          <Link to="/feedback" className={style.mainPage__card_feedback_inner}>
-            <div className={style.mainPage__card_feedback_img}>
-              <img src={imgFeedback} alt="Feedback_Image" />
+          <div className={style.mainPage__card_feedback}>
+            <Link to="/feedback" className={style.mainPage__card_feedback_inner}>
+              <div className={style.mainPage__card_feedback_img}>
+                <img src={imgFeedback} alt="Feedback_Image" />
+              </div>
+            </Link>
+            <div className={style.mainPage__card_feedback_info}>
+              <p>Обратная связь</p>
             </div>
-          </Link>
-          <div className={style.mainPage__card_feedback_info}>
-            <p>Обратная связь</p>
           </div>
-        </div>
-
+        </>
+        )}
+        {(user.role_id) && (
         <div className={style.mainPage__card_feedback}>
           <Link to="/lostbutton" className={style.mainPage__card_feedback_inner}>
             <div className={style.mainPage__card_feedback_img}>
@@ -47,6 +57,7 @@ function MainPage() {
             <p>Я на лекции</p>
           </div>
         </div>
+        )}
 
       </div>
     </>
--- frontend/src/Components/Menu/Menu.jsx
+++ frontend/src/Components/Menu/Menu.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import style from './Menu.module.css';
 
 function Menu() {
+  const { user } = useSelector((state) => state);
   return (
     <div className={style.menu__container}>
-      <ul className={style.menu__list}>
-        <li className={style.menu__item}>
-          <Link to="/progress" className={style.menu__link}>Progress</Link>
-        </li>
-        <li className={style.menu__item}>
-          <Link to="/myprogress" className={style.menu__link}>My progress</Link>
-        </li>
-        <li className={style.menu__item}>
-          <Link to="/feedback" className={style.menu__link}>Feedback</Link>
-        </li>
-        <li className={style.menu__item}>
-          <Link to="/myfeedback" className={style.menu__link}>My feedback</Link>
-        </li>
-        <li className={style.menu__item}>
-          <Link to="/lostbutton" className={style.menu__link}>Lost button</Link>
-        </li>
-      </ul>
+      {(user.role_id) && (
+        <ul className={style.menu__list}>
+          {(user.role_id === 2) && (
+          <>
+            <li className={style.menu__item}>
+              <Link to="/progress" className={style.menu__link}>Progress</Link>
+            </li>
+            <li className={style.menu__item}>
+              <Link to="/myprogress" className={style.menu__link}>My progress</Link>
+            </li>
+            <li className={style.menu__item}>
+              <Link to="/feedback" className={style.menu__link}>Feedback</Link>
+            </li>
+            <li className={style.menu__item}>
+              <Link to="/myfeedback" className={style.menu__link}>My feedback</Link>
+            </li>
+          </>
+          )}
+          <li className={style.menu__item}>
+            <Link to="/lostbutton" className={style.menu__link}>Lost button</Link>
+          </li>
+        </ul>
+      )}
     </div>
+
   );
 }
 
